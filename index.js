cc2c32bb34c2620f5a591ce17f2ab97ecff66207
Gustavo <gustavo.oliveira19@estudante.ufla.br>
2026-05-16 10:15:36 -0300

    feat: update message to test automated deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Infra Pipeline Online!');
+    res.status(200).send('Testando automatização de deploy com render!');
 });
 
-module.exports = app; // Exportamos para o teste
+module.exports = app; 
