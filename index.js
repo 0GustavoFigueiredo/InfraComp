ead250687763c276d831045a76f70c10b9b1c9e5
Gustavo <gustavo.oliveira19@estudante.ufla.br>
2026-05-24 10:34:53 -0300

    teste para video de infra

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Demonstrando deploy automático!');
+    res.status(200).send('Demonstrando pipeline and deployment com Render!');
 });
 
 module.exports = app; 
